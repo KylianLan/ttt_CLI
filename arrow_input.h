11b2cfd1f485fad42cd01bfdfbdff79ffb531dc2
Kylian Langlois <kylianlanglois1@gmail.com>
2026-05-08 13:00:28 +0200

    Updated documentation and variable name

--- arrow_input.h
+++ arrow_input.h
@@ -5,11 +5,7 @@
 
 void set_raw_mode(struct termios *orig_termios);
 void reset_mode(struct termios *orig_termios);
-<<<<<<< HEAD
-int read_arrows(void);
-=======
 int read_arrows(int enter_key);
->>>>>>> game
 
 #endif
 
