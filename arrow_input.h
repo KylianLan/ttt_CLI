d3f96c9496bfe5e27e10ec928bb72354cd6b7cc3
Kylian Langlois <kylianlanglois1@gmail.com>
2026-05-08 12:16:07 +0200

    Merged with game branch, as I worked on the input in this branch

--- arrow_input.h
+++ arrow_input.h
@@ -5,7 +5,11 @@
 
 void set_raw_mode(struct termios *orig_termios);
 void reset_mode(struct termios *orig_termios);
+<<<<<<< HEAD
 int read_arrows(void);
+=======
+int read_arrows(int enter_key);
+>>>>>>> game
 
 #endif
 
--- main.c
+++ main.c
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 #include "input.c"
 #include "arrow_input.c"
 
 int main(void) {
     read_arrows();
+=======
+#include "menu.h"
+
+int main(void) {
+    show_menu();
+>>>>>>> game
     return 0;
 }
